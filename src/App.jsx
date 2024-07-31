9ebc765bc470459550cbdab29c2e8cde55446713
LITKHRISS <mordangmack@proton.me>
2024-07-30 21:33:35 -0400

    Updates to navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import { useEffect, useState } from 'react'
 import './App.css'
-
-const NAVIGATION_EVENT = 'pushstate'
+import { EVENTS } from './consts.js'
 
 function navigate (href) {
   window.history.pushState({}, '', href)
   // crear evento perzonalizado
-  const navigationEvent = new Event(NAVIGATION_EVENT)
+  const navigationEvent = new Event(EVENTS.PUSHSTATE)
   window.dispatchEvent(navigationEvent)
 }
 
@@ -42,10 +41,12 @@ function App() {
       setCurrentPath(window.location.pathname)
     }
 
-    window.addEventListener(NAVIGATION_EVENT, onLocationChange)
+    window.addEventListener(EVENTS.PUSHSTATE, onLocationChange)
+    window.addEventListener(EVENTS.POPSTATE, onLocationChange)
 
     return () => {
-      window.removeEventListener(NAVIGATION_EVENT, onLocationChange)
+      window.removeEventListener(EVENTS.PUSHSTATE, onLocationChange)
+      window.removeEventListener(EVENTS.POPSTATE, onLocationChange)
     }
   }, [])
 
