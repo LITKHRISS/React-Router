91180bc186332b036de208774ddee02b3667e813
LITKHRISS <mordangmack@proton.me>
2024-07-30 21:24:01 -0400

    Navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+
+const NAVIGATION_EVENT = 'pushstate'
+
+function navigate (href) {
+  window.history.pushState({}, '', href)
+  // crear evento perzonalizado
+  const navigationEvent = new Event(NAVIGATION_EVENT)
+  window.dispatchEvent(navigationEvent)
+}
 
 function HomePage () {
   return (
     <>
     <h1>Home</h1>
     <p>Pagina de ejemplo para crear un React Router desde cero</p>
-    <a href='/about'> Ir a sobre nosotros </a>
+    <button onClick={() => navigate('/about')} > Ir a sobre nosotros </button>
     </>
   )
 }
@@ -20,13 +29,25 @@ function AboutPage () {
     <p>Hola soy Christopher y estoy creando un clon de React Router</p>
     </div>
 
-    <a href='/'> Ir a home </a>
+    <button onClick={() => navigate('/')}> Ir a home </button>
     </>
   )
 }
 
 function App() {
   const [currentPath, setCurrentPath] = useState(window.location.pathname)
+
+  useEffect(() => {
+    const onLocationChange = () => {
+      setCurrentPath(window.location.pathname)
+    }
+
+    window.addEventListener(NAVIGATION_EVENT, onLocationChange)
+
+    return () => {
+      window.removeEventListener(NAVIGATION_EVENT, onLocationChange)
+    }
+  }, [])
 
 
   return (
