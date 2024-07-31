71e02fea8ab37316990d42e2f892dc591f96043c
LITKHRISS <mordangmack@proton.me>
2024-07-30 21:52:50 -0400

    Improvements

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,8 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import { EVENTS } from './consts.js'
-
-function navigate (href) {
-  window.history.pushState({}, '', href)
-  // crear evento perzonalizado
-  const navigationEvent = new Event(EVENTS.PUSHSTATE)
-  window.dispatchEvent(navigationEvent)
-}
-
-function HomePage () {
-  return (
-    <>
-    <h1>Home</h1>
-    <p>Pagina de ejemplo para crear un React Router desde cero</p>
-    <button onClick={() => navigate('/about')} > Ir a sobre nosotros </button>
-    </>
-  )
-}
-
-function AboutPage () {
-  return (
-    <>
-    <h1>About</h1>
-    <div>
-      <img src='src/resourses/IMG_6611.jpeg' alt='Foto de Christopher' />
-    <p>Hola soy Christopher y estoy creando un clon de React Router</p>
-    </div>
-
-    <button onClick={() => navigate('/')}> Ir a home </button>
-    </>
-  )
-}
+import HomePage from './pages/Home.jsx'
+import AboutPage from './pages/About.jsx'
 
 function App() {
   const [currentPath, setCurrentPath] = useState(window.location.pathname)
@@ -49,7 +20,6 @@ function App() {
       window.removeEventListener(EVENTS.POPSTATE, onLocationChange)
     }
   }, [])
-
 
   return (
     <main>
---
+++ src/pages/About.jsx
@@ -0,0 +1,15 @@
+import { navigate } from '../Link.jsx'
+
+export default function AboutPage () {
+    return (
+      <>
+      <h1>About</h1>
+      <div>
+        <img src='src/resourses/IMG_6611.jpeg' alt='Foto de Christopher' />
+      <p>Hola soy Christopher y estoy creando un clon de React Router</p>
+      </div>
+  
+      <button onClick={() => navigate('/')}> Ir a home </button>
+      </>
+    )
+  }
