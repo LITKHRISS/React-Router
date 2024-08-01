ba88a80663498996c643e4b0a2ef3a5227d994e1
LITKHRISS <mordangmack@proton.me>
2024-07-31 22:20:40 -0400

    USING link as SPA instead of using buttons

--- src/Link.jsx
+++ src/Link.jsx
@@ -7,8 +7,18 @@ export function navigate (href) {
   }
 
   export function Link ({ target, to, ...props }) {
-    const handleClick = () => {
-        navigate(to)
+    const handleClick = (event) => {
+       
+
+        const isMainEvent = event.button == 0 // primary click
+        const isModifiedEvent = event.metakey || event.altKey || event. ctrlKey || event.shiftKey
+        const isManageableEvent = target == undefined || target == '_self'
+
+        if (isMainEvent && isManageableEvent && !isModifiedEvent) {
+            event.preventDefault()
+            navigate(to) // navegacion con SPA
+        }
+       
     }
 
     return <a onClick={handleClick} href={to} target={target} {...props} />
