51154857d6602a5bbc1c4401c2135b15f7edc5fb
LITKHRISS <mordangmack@proton.me>
2024-08-12 21:34:27 -0400

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,27 @@
 import './App.css'
 import HomePage from './pages/Home.jsx'
 import AboutPage from './pages/About.jsx'
+import Page404 from './pages/404.jsx'
+import SearchPage from './pages/Search.jsx'
 
 import { Router } from './Router.jsx'
-import Page404 from './pages/404.jsx'
+import { Route } from './Route.jsx'
 
-const appRoutes = [
-  {
-    path: '/',
-    Component: HomePage
-  },
-  {
-    path: '/about',
-    Component: AboutPage
-  },
+
+const appRoutes = [ 
   {
     path: '/search/:query', // /search/javascript /search/python /search/react
-    Component: ({routeParams}) => <h1>Has buscado {routeParams.query}</h1>
+    Component: SearchPage
   }
 ]
 
 function App() {
   return (
     <main>
-      <Router routes={appRoutes} defaultComponent={Page404} />
+      <Router routes={appRoutes} defaultComponent={Page404}>
+      <Route path='/' Component={HomePage} />
+      <Route path='/about' Component={AboutPage} />
+      </Router> 
     </main>
   )
 }
